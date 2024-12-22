3a3c0336edd60b00eeadbc087588a532a5c87919
Sidney <sid.14boys@yahoo.fr>
2024-12-21 18:05:37 -0800

    Add CI/CD workflow for Next.js application; implement build, test, and deployment steps for preview and production environments

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -42,15 +42,15 @@ const Hero: React.FC<HeroProps> = ({ className = "" }) => {
 
     try {
       await emailjs.send(
-        "service_66eglml", // from EmailJS
-        "template_xlm0fe7", // from EmailJS
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
         {
           from_name: formData.name,
           from_email: formData.email,
           message: formData.message,
           to_name: "Sidney",
         },
-        "AoHsOOb2pr57gDes4" // from EmailJS
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
       );
 
       alert("Message sent successfully!");
